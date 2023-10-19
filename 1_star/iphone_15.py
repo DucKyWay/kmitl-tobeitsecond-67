b76d1872754f494bc95d5cb71716bb480671008d
DucKyWay <niji2549@gmail.com>
2023-10-19 18:19:49 +0700

    finish iphone15

--- 1_star/iphone_15.py
+++ 1_star/iphone_15.py
@@ -1,29 +1,37 @@
-# iPhone 15 มีหลายรุ่นดังนี้ iPhone 15, iPhone 15 Plus, iPhone 15 Pro, iPhone 15 Pro Max และมีหลายความจุแต่ร้านพี่ที่นี้ มีให้แค่ 2 ความจุคือ 128 GB และ 256 GB
-
-# iPhone 15 128 GB ราคา 32900 บาท
-# iPhone 15 256 GB ราคา 36900 บาท
-# iPhone 15 Plus 128 GB ราคา 37900 บาท
-# iPhone 15 Plus 256 GB ราคา 41900 บาท
-# iPhone 15 Pro 128 GB ราคา 41900 บาท
-# iPhone 15 Pro 256 GB ราคา 45900 บาท
-# iPhone 15 Pro Max 256 GB ราคา 48900 บาท
-
-# ให้หาราคาของ iPhone ตาม spec ที่ลูกค้าต้องการ
-
-# Input Specification
-# 2 บรรทัด :
-
-# บรรทัดที่ 1 รุ่นของโทรศัพท์ เป็น String
-
-# บรรทัดที่ 2 ความจุที่ต้องการ เป็น String
-
-# Output Specification
-# 1 บรรทัดเป็นราคาของ iPhone ถ้าให้รุ่นกับความจุมาไม่ตรงกับของในร้านให้ตอบเป็น Not Sell แทน
-
 '''Hello <i>Judge'''
 
 def main():
     '''Main Function'''
-    # Write your code here
+    model = str(input())
+    storage = str(input())
+
+    if model == "iPhone 15":
+        if storage == "128 GB":
+            print(32900)
+        elif storage == "256 GB":
+            print(36900)
+        else:
+            print("Not Sell")
+    elif model == "iPhone 15 Plus":
+        if storage == "128 GB":
+            print(37900)
+        elif storage == "256 GB":
+            print(41900)
+        else:
+            print("Not Sell")
+    elif model == "iPhone 15 Pro":
+        if storage == "128 GB":
+            print(41900)
+        elif storage == "256 GB":
+            print(45900)
+        else:
+            print("Not Sell")
+    elif model == "iPhone 15 Pro Max":
+        if storage == "256 GB":
+            print(48900)
+        else:
+            print("Not Sell")
+    else:
+            print("Not Sell")
 
 main()
