730131b707397c8d7c9cb4b552ddeaa0a69a6e99
DucKyWay <niji2549@gmail.com>
2023-10-19 17:28:37 +0700

    finish grade

--- 0_star/grade.py
+++ 0_star/grade.py
@@ -3,20 +3,19 @@
 def main():
     '''Main Function'''
     score = float(input())
-    scores = (score >= 0) * (score <= 100) * round(score, 2)
-    if scores >= 80:
+    if score >= 80:
         print("A")
-    elif scores < 80 and scores >= 75:
+    elif score < 80 and score >= 75:
         print("B+")
-    elif scores < 75 and scores >= 70:
+    elif score < 75 and score >= 70:
         print("B")
-    elif scores < 70 and scores >= 65:
+    elif score < 70 and score >= 65:
         print("C+")
-    elif scores < 65 and scores >= 60:
+    elif score < 65 and score >= 60:
         print("C")
-    elif scores < 60 and scores >= 55:
+    elif score < 60 and score >= 55:
         print("D+")
-    elif scores < 55 and scores >= 50:
+    elif score < 55 and score >= 50:
         print("D")
     else:
         print("F")
