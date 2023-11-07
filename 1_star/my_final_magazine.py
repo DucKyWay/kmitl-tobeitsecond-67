6742fdc5adc3643f1a19ee3b53e9df98648235d2
DucKyWay <niji2549@gmail.com>
2023-11-07 13:49:47 +0700

    change devices

--- 1_star/my_final_magazine.py
+++ 1_star/my_final_magazine.py
@@ -3,13 +3,12 @@ enemy_hp = float(input()) # เลือดของศัตรู
 gun_damage = float(input()) # ความเสียหายของกระสุน 1 นัด
 hp_remain = enemy_hp
 
-for _ in range(bullet):
+for i in range(bullet):
     hp_remain -= gun_damage
+    print(i+1, bullet-(i+1))
     if hp_remain <= 0:
-        result = "dead : " + str(bullet - _ - 1) + " bullet remain"
-        # result = "dead : " + str(bullet - (_ + 1)) + " bullet remain"
+        result = "dead : " + str(bullet - (i + 1)) + " bullet remain"
         break
 else:
     result = "alive : " + str(hp_remain) + " health"
-
 print(result)
