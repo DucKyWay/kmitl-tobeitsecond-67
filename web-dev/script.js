6043241bf443ff72c5b46ae99fdfeb918bab339b
DucKyWay <niji2549@gmail.com>
2023-11-11 20:11:07 +0700

    finish web

--- web-dev/script.js
+++ web-dev/script.js
@@ -1,6 +1,6 @@
 const themeToggle = document.getElementById("theme-toggle");
-        const body = document.body;
+const body = document.body;
 
-        themeToggle.addEventListener("click", () => {
-            body.classList.toggle("theme-toggle");
-        });
+themeToggle.addEventListener("click", () => {
+    body.classList.toggle("theme-toggle");
+});
